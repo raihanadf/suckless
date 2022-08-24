665f892e41067497cfc8c0fb8de0a309e75b2eac
Raihan <17radf@gmail.com>
2022-08-24 10:52:09 +0700

    update: actually, dont autostart steam and discord

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int snap           = 32;  /* snap pixel */
 static const unsigned int gappih         = 10;  /* horiz inner gap between windows */
 static const unsigned int gappiv         = 10;  /* vert inner gap between windows */
 static const unsigned int gappoh         = 8;  /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov         = 28;  /* vert outer gap between windows and screen edge */
+static const unsigned int gappov         = 24;  /* vert outer gap between windows and screen edge */
 static const int smartgaps_fact          = 1;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
 static const int showbar                 = 1;   /* 0 means no bar */
 static const int topbar                  = 1;   /* 0 means bottom bar */
@@ -97,8 +97,8 @@ static char *colors[][ColCount] = {
 static const char *const autostart[] = {
 	"nitrogen", "--restore", NULL,
 	"sxhkd", NULL,
-	"discord", "--start-minimized", NULL,
-	"steam", "-silent", NULL,
+	/* "discord", "--start-minimized", NULL, */
+	/* "steam", "-silent", NULL, */
 	/* "picom", "--experimental-backend", "--backend", "glx", "--xrender-sync-fence", NULL, */
 	"sh","-c","$HOME/.dotfiles/scripts/autostart.sh", NULL,
 	"sh","-c","$HOME/.dotfiles/scripts/batteryCheck.sh", NULL,
