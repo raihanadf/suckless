cee7324773190a4e0c3445f0311d7f02ba177983
Raihan <17radf@gmail.com>
2024-04-22 01:59:42 +0700

    feat: switch back to gaps and top bar

--- dwm/config.h
+++ dwm/config.h
@@ -3,13 +3,13 @@
 /* appearance */
 static const unsigned int borderpx       = 2;   /* border pixel of windows */
 static const unsigned int snap           = 32;  /* snap pixel */
-static const unsigned int gappih         = 0;  /* horiz inner gap between windows */
-static const unsigned int gappiv         = 0;  /* vert inner gap between windows */
-static const unsigned int gappoh         = 0;  /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov         = 0;  /* vert outer gap between windows and screen edge */
+static const unsigned int gappih         = 5;  /* horiz inner gap between windows */
+static const unsigned int gappiv         = 5;  /* vert inner gap between windows */
+static const unsigned int gappoh         = 5;  /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov         = 5;  /* vert outer gap between windows and screen edge */
 static const int smartgaps_fact          = 1;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
 static const int showbar                 = 1;   /* 0 means no bar */
-static const int topbar                  = 0;   /* 0 means bottom bar */
+static const int topbar                  = 1;   /* 0 means bottom bar */
 static const int bar_height              = 0;   /* 0 means derive from font, >= 1 explicit height */
 static const int focusonwheel            = 0;
 /* Status is to be shown on: -1 (all monitors), 0 (a specific monitor by index), 'A' (active monitor) */
